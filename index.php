2e44aab9ae2ab838f827f09da0cd335ee1678df6
Christophe LARUE <dev@startx.fr>
2023-10-01 15:53:21 +0200

    ajout de la version devel

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
-<h1> STARTX test 55555 </h1>
-
-
-<?php phpinfo();
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ma Page</title>
+    <link rel="stylesheet" href="./styles.css">
+</head>
+<body class="devel">
+    <div class="overlay"></div>
+    <h1>Version de development</h1>
+</body>
+</html>
